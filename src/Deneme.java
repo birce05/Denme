97b484affb91419c5b8e469d24f3b11e785ea2d2
birce05 <birsenberrin@gmail.com>
2021-04-29 23:38:20 +0300

    .

---
+++ src/Deneme.java
@@ -0,0 +1,6 @@
+
+public class Deneme {
+//yeni birþey yapýnca ++tusuna baþýn alta atsýn
+	//alta attý gördünmüok
+	//sonra commit and push ' a týklayýn
+}
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		// TODO Auto-generated method stub
 //simdi dikkatli izle
 		//burada týkladýðým yerlere bak
+		//deðisiklik yapýnca beni takip et
 	}
 
 }
